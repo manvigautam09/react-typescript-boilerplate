50b2926c182acb741319480a766ac8f4ccb59c44
manvigautam09 <mnvsgautam9@gmail.com>
2021-01-23 01:10:57 +0530

    fixed the issue with routing

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import history from "../../history";
+import { useHistory } from "react-router-dom";
 import { LOGIN_ROUTE } from "../../utils/routeConstants";
 
 const Dashboard = () => {
+  const history = useHistory();
   const handleSignOut = () => {
     localStorage.clear();
     history.push(LOGIN_ROUTE);
