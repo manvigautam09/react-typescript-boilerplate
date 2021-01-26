5534f76b44a074bf7b841e8e5f18936e63582820
manvigautam09 <mnvsgautam9@gmail.com>
2021-01-26 22:04:07 +0530

    styled boilerplate with tailwind css

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -18,7 +18,10 @@ const Login = () => {
         value={password}
         onChange={(event) => setPassword(event.target.value)}
       />
-      <button onClick={() => userLoginRequestHandler({ userName, password })}>
+      <button
+        onClick={() => userLoginRequestHandler({ userName, password })}
+        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      >
         {loginSpinner ? "Logging in" : "Submit"}
       </button>
     </div>
